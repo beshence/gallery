395e3bcbdf74c5a649a01cd6c4d30cce9eb6dfff
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2026-08-07 01:18:33 +0300

    update dependencies, set dynamic height of appbar and update avatar button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,37 +18,17 @@ class _HomeScreenState extends State<HomeScreen> {
             actionsPadding: .only(right: 8),
             actions: [
               Beshence.selectedAccount!.avatarButton(
-                  context: context,
-                  onPressed: () {
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (context) => SingleChildScrollView(
-                            child: SafeArea(
-                                child: Padding(
-                                    padding: const EdgeInsets.all(32.0),
-                                    child: Column(
-                                      children: [
-                                        ListTile(
-                                          contentPadding: EdgeInsets.all(0),
-                                          leading: Icon(Icons.settings),
-                                          title: RichText(
-                                              text: TextSpan(
-                                                  style: Theme.of(context).textTheme.bodyLarge,
-                                                  children: [
-                                                    TextSpan(text: "Beshence Gallery settings")
-                                                  ]
-                                              )
-                                          ),
-                                          onTap: () => context.push("/settings"),
-                                        ),
-                                      ],
-                                    )
-                                )
-                            )
-                        )
-                    );
-                  }
-              )
+                  onPressed: () => BeshenceWidgets.showAccountChooserModal(
+                      context: context,
+                      children: [
+                        BeshenceWidgets.accountChooserTile(
+                            title: "Beshence Gallery settings",
+                            leading: Icon(Icons.settings),
+                            onTap: () => context.push("/settings")
+                        ),
+                      ]
+                  )
+              ),
             ]
         ),
         body: SafeArea(
